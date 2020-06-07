df241ab7741183d0f7548976a7fb1378dafc39d9
rungo728 <rufii72865@gmail.com>
2020-06-07 20:16:50 +0900

    create header and navmenu
    bootstarpでヘッダーとナビを作成

--- wp-content/themes/wordpressblog/index.php
+++ wp-content/themes/wordpressblog/index.php
@@ -8,10 +8,48 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>KAblog</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <header>
+      <div class="container">
+        <h1 class="h1 py-3">KAblog</h1> 
+      </div>
+    </header>
+    <!-- グローバルナビ -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <div class="container">
+        <a class="navbar-brand text-white" href="#">ホーム</a>
+        <button class="navbar-toggler bg-white" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link text-white" href="#">書きてについて</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link text-white" href="#">WEbデザイン</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link text-white" href="#">Webコーディング</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link text-white" href="#">どうが</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link text-white" href="#">お問合せ</a>
+            </li>
+          </ul>
+        </div>      
+      </div>
+    </nav>
+    <main class="">
+      <div class="container">
+      </div>
+    </main>
+
+
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
