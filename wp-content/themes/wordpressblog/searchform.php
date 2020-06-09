f4a3bb8c659f0f57754a18146a1805aebac381fa
rungo728 <rufii72865@gmail.com>
2020-06-09 21:59:38 +0900

    Create searchform.php
    検索フォームが機能するように設定

---
+++ wp-content/themes/wordpressblog/searchform.php
@@ -0,0 +1,6 @@
+<!-- formactionはホームの設定？ -->
+<!-- 検索フォームで検索できるようにする設定 -->
+<form action="<?php home_url();?>" method="get">
+
+<input name="s" type="text" class="form-control" placeholder="検索できます">
+</form>
