de279cfe48218d945941bbef25436d5efb77ab8c
rungo728 <rufii72865@gmail.com>
2020-06-10 18:00:37 +0900

    commentout

--- wp-content/themes/wordpressblog/index.php
+++ wp-content/themes/wordpressblog/index.php
@@ -82,9 +82,12 @@
       <!-- フッター部分 -->
       <?php get_footer(); ?>
     </main>
+    <!-- 一番上に固定ツールバーを出現させるための記述 -->
+    <?php wp_footer(); ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script
+    <!-- function.phpに設定済みなのでコメントアウト -->
+    <!-- <script
       src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
       integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
       crossorigin="anonymous"
@@ -98,6 +101,6 @@
       src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
       integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
       crossorigin="anonymous"
-    ></script>
+    ></script> -->
   </body>
 </html>
