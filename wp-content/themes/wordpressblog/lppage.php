98ec446ce3efbd1da282697cd65fb86c69032450
rungo728 <rufii72865@gmail.com>
2020-06-20 06:05:15 +0900

    Update lppage.php

--- wp-content/themes/wordpressblog/lppage.php
+++ wp-content/themes/wordpressblog/lppage.php
@@ -3,3 +3,54 @@
 Template Name: ランディングページ
 */
 ?>
+<?php get_header(); ?>
+    <main class="bg-light">
+      <div class="container">
+        <div class="row py-3">
+          <!-- メインコンテンツ -->
+          <div class="col-12 mx-auto">
+            <!-- 記事があれば繰り返し処理で一覧表示する -->
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+              <div class="bg-white pb-5 mb-5 text-center">
+                <!-- サムネイル -->
+                <div class="pb-3">
+                  <?php if ( has_post_thumbnail() ) : ?>
+                    <?php the_post_thumbnail('', array( 'class' => 'img-fluid' )); ?>
+                  <?php else : ?>
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/no-image.png" alt="" class="img-fluid" />
+                  <?php endif; ?>
+                </div>
+                <!-- 記事タイトル -->
+                <h1 class="h2 px-3 pb-3 font-weight-bolder"><?php the_title(); ?></h1>
+                <!-- 本文 -->
+                <!-- 本文の情報を全て取得できる -->
+                <div class="test-left px-2"><?php the_content(); ?></div>
+              </div>
+            <?php endwhile; else : ?>
+              <p>記事がありません。</p>
+            <?php endif; ?>
+          </div>
+        </div>
+      </div>
+      <!-- フッター部分 -->
+      <?php get_footer(); ?>
+    </main>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script
+      src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+      integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
