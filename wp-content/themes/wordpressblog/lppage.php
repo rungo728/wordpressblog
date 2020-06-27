cab0b911289801d8d4df788e4a233401fa7c5de7
rungo728 <rufii72865@gmail.com>
2020-06-27 12:20:56 +0900

    add user voice
    お客様の声の部分をhtml css作成

--- wp-content/themes/wordpressblog/lppage.php
+++ wp-content/themes/wordpressblog/lppage.php
@@ -9,16 +9,9 @@ Template Name: ランディングページ
         <div class="row py-3">
           <!-- メインコンテンツ -->
           <div class="col-12 mx-auto">
-            <!-- 記事があれば繰り返し処理で一覧表示する -->
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
               <div class="bg-white pb-5 mb-5 text-center">
                 <!-- サムネイル -->
                 <div class="pb-3" id="maining">
-                  <?php if ( has_post_thumbnail() ) : ?>
-                    <?php the_post_thumbnail('', array( 'class' => 'img-fluid' )); ?>
-                  <?php else : ?>
-                    <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/no-image.png" alt="" class="img-fluid" /> -->
-                  <?php endif; ?>
                   <p class="mvCatch text-left">
                     <span class="lineWavecmn01 font-weight-bolder">
                       英語にたくさん
@@ -32,14 +25,67 @@ Template Name: ランディングページ
                       楽しく学ぶための広場。
                     </span>
                   </p>
-                  <p class="col-6 mvAnthor">
+                  <p class="mb-5 col-6 mvAnthor">
                     <a href="#point" class="fuwafuwa">
-                      <img src="<?php echo get_template_directory_uri(); ?>/images/kaonline.png" alt="">
+                      <img src="<?php echo get_template_directory_uri(); ?>/images/logo2.png" alt="">
                     </a>
                   </p>
                 </div>
+                <div id="contents" class="mt-5">
+                  <main role="main">
+                    <div id="main">
+                      <section class="h6 secTrouble js-inview fadeIn">
+                        <div class="troubleIn cf">
+                          <h2 class="troubleTit font-weight-bolder">
+                            こんなことで
+                            <br class="viewSp">
+                            お悩みではありませんか？
+                          </h2>
+                          <div class="d-inline-flex troubleVoice">
+                            <div class="mr-4 troubleVoiceItem item01">
+                              <p class="p-4 troubleVoiceTxt font-weight-bolder text-white bg-info">
+                                抵抗なく英語に
+                                <br>
+                                接してほしい...
+                              </p>
+                              <div class="troubleVoiceImg">
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/img_trouble01.png" alt="">
+                              </div>
+                            </div>
+                            <div class="mr-4 troubleVoiceItem item02">
+                              <p class="p-4 troubleVoiceTxt font-weight-bolder text-white bg-info">
+                                中学での英語学習で、
+                                <br>
+                                出遅れたくない…
+                              </p>
+                              <div class="troubleVoiceImg">
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/img_trouble02.png" alt="">
+                              </div>
+                            </div>
+                            <div class="troubleVoiceItem item03">
+                              <p class="p-4 troubleVoiceTxt font-weight-bolder text-white bg-info">
+                                英語を話せても、
+                                <br>
+                                書く力が足りない気がする…
+                              </p>
+                              <div class="troubleVoiceImg">
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/img_trouble03.png" alt="">
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </section>
+                    </div>
+                  </main>
+                </div>
+              </div>
+            </div>
+          <!-- メインコンテンツ -->
+          <div class="col-12 mx-auto">
+            <!-- 記事があれば繰り返し処理で一覧表示する -->
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <!-- 記事タイトル -->
-                <h1 class="h2 px-3 pb-3 font-weight-bolder"><?php the_title(); ?></h1>
+                <h1 class="h2 px-3 py-5 font-weight-bolder"><?php the_title(); ?></h1>
                 <!-- 本文 -->
                 <!-- 本文の情報を全て取得できる -->
                 <div class="test-left px-2"><?php the_content(); ?></div>
