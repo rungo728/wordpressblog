bef37c71c523c653656c44e0902b7d3868015369
rungo728 <rufii72865@gmail.com>
2020-08-16 17:44:59 +0900

    Update lppage2.php
    PC版のヘッダーを作成

--- wp-content/themes/wordpressblog/lppage2.php
+++ wp-content/themes/wordpressblog/lppage2.php
@@ -3,3 +3,47 @@
 Template Name: ランディングページ２
 */
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/lppage2.css" />
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/reset.css" />
+  <title>KA FREELANCE
+  </title>
+</head>
+<body>
+  <header>
+    <h1 class="header-ttl">
+      <img width="240" height="160" src="<?php echo get_template_directory_uri(); ?>/images/kafreelance2.png" alt="KAFREELANCE">
+    </h1>
+    <nav class="header-nav">
+      <ul class="headernavList">
+        <li class="header-navItem">
+          <a href="#news" class="header-navLink js-navLink">News</a>
+        </li>
+        <li class="header-navItem">
+          <a href="#service" class="header-navLink js-navLink">Service</a>
+        </li>
+        <li class="header-navItem">
+          <a href="#results" class="header-navLink js-navLink">Results</a>
+        </li>
+        <li class="header-navItem">
+          <a href="#price" class="header-navLink js-navLink">Price</a>
+        </li>
+        <li class="header-navItem">
+          <a href="#comments" class="header-navLink js-navLink">Comments</a>
+        </li>
+        <li class="header-navItem">
+          <a href="#faqs" class="header-navLink js-navLink">FAQs</a>
+        </li>
+        <li class="header-navItem">
+          <a href="#contacts" class="header-navLink js-navLink">Contacts</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  
+</body>
+</html>
